96b8f5acd2ca3a6e1238fb5cb8667fec974fb504
K3rnys <rainykernys@gmail.com>
2024-11-21 18:37:09 +0300

    1

---
+++ source/dishes.js
@@ -0,0 +1,272 @@
+export const products = [
+    {
+      keyword: 'gaspacho',
+      name: 'Гаспачо',
+      price: 195,
+      category: 'soup',
+      weight: '350 г',
+      image: 'source/img/gaspacho.jpg',
+      kind: 'veg'
+    },
+    {
+      keyword: 'gribnoy',
+      name: 'Грибной суп-пюре',
+      price: 185,
+      category: 'soup',
+      weight: '330 г',
+      image: 'source/img/gribnoy.jpg',
+      kind: 'veg'
+    },
+    {
+      keyword: 'norwegian',
+      name: 'Норвежский суп',
+      price: 270,
+      category: 'soup',
+      weight: '330 г',
+      image: 'source/img/norway.jpg',
+      kind: 'fish'
+    },
+    {
+      keyword: 'ramen',
+      name: 'Рамен',
+      price: 375,
+      category: 'soup',
+      weight: '425 г',
+      image: 'source/img/ramen.jpg',
+      kind: 'meat'
+    },
+    {
+      keyword: 'tomyam',
+      name: 'Том ям с креветками',
+      price: 650,
+      category: 'soup',
+      weight: '500 г',
+      image: 'source/img/tomyam.jpg',
+      kind: 'fish'
+    },
+    {
+      keyword: 'chickensoup',
+      name: 'Куриный суп',
+      price: 330,
+      category: 'soup',
+      weight: '350 г',
+      image: 'source/img/chickensoup.jpg',
+      kind: 'meat'
+    },
+    {
+      keyword: 'kartoha',
+      name: 'Жаренная картошка с грибами',
+      price: 150,
+      category: 'maindish',
+      weight: '250 г',
+      image: 'source/img/kartoha.jpg',
+      kind: 'veg'
+    },
+    {
+      keyword: 'lasagna',
+      name: 'Лазанья',
+      price: 385,
+      category: 'maindish',
+      weight: '310 г',
+      image: 'source/img/lazagna.jpg',
+      kind: 'meat'
+    },
+    {
+      keyword: 'kotleti',
+      name: 'Котлеты из курицы с картофельным пюре',
+      price: 225,
+      category: 'maindish',
+      weight: '280 г',
+      image: 'source/img/kotleti.jpg',
+      kind: 'meat'
+    },
+    {
+      keyword: 'vegpizza',
+      name: 'Вегетарианская пицца',
+      price: 500,
+      category: 'maindish',
+      weight: '350 г',
+      image: 'source/img/vegpizza.jpg',
+      kind: 'veg'
+    },
+    {
+      keyword: 'losos',
+      name: 'Запеченный лосось',
+      price: 990,
+      category: 'maindish',
+      weight: '200 г',
+      image: 'source/img/losos.jpg',
+      kind: 'fish'
+    },
+    {
+      keyword: 'fishkotleti',
+      name: 'Рыбные котлеты',
+      price: 650,
+      category: 'maindish',
+      weight: '300 г',
+      image: 'source/img/fishkotleti.jpg',
+      kind: 'fish'
+    },
+    {
+      keyword: 'koreansalad',
+      name: 'Корейский салат с овощами и яйцом',
+      price: 330,
+      category: 'salad',
+      weight: '250 г',
+      image: 'source/img/koreansalad.jpg',
+      kind: 'veg'
+    },
+    {
+      keyword: 'cesarchicken',
+      name: 'Цезарь с цыпленком',
+      price: 370,
+      category: 'salad',
+      weight: '220 г',
+      image: 'source/img/cesarchicken.jpg',
+      kind: 'meat'
+    },
+    {
+      keyword: 'kapreze',
+      name: 'Капрезе с моцареллой',
+      price: 350,
+      category: 'salad',
+      weight: '235 г',
+      image: 'source/img/kapreze.jpg',
+      kind: 'veg'
+    },
+    {
+      keyword: 'tunec',
+      name: 'Салат с тунцом',
+      price: 480,
+      category: 'salad',
+      weight: '250 г',
+      image: 'source/img/tunec.jpg',
+      kind: 'fish'
+    },
+    {
+      keyword: 'potatocesar',
+      name: 'Картофель фри с соусом цезарь',
+      price: 280,
+      category: 'salad',
+      weight: '235 г',
+      image: 'source/img/potatocesar.jpg',
+      kind: 'veg'
+    },
+    {
+      keyword: 'potatoketchup',
+      name: 'Картофель фри с кетчупом',
+      price: 260,
+      category: 'salad',
+      weight: '235 г',
+      image: 'source/img/potatoketchup.jpg',
+      kind: 'veg'
+    },
+    {
+      keyword: 'apelsin',
+      name: 'Апельсиновый сок',
+      price: 120,
+      category: 'drink',
+      weight: '300 мл',
+      image: 'source/img/apelsin.jpg',
+      kind: 'cold'
+    },
+    {
+      keyword: 'yablochni',
+      name: 'Яблочный сок',
+      price: 90,
+      category: 'drink',
+      weight: '300 мл',
+      image: 'source/img/yablochni.jpg',
+      kind: 'cold'
+    },
+    {
+      keyword: 'morkovni',
+      name: 'Морковный сок',
+      price: 110,
+      category: 'drink',
+      weight: '300 мл',
+      image: 'source/img/morkovni.jpg',
+      kind: 'cold'
+    },
+    {
+      keyword: 'kapuchino',
+      name: 'Капучино',
+      price: 180,
+      category: 'drink',
+      weight: '300 мл',
+      image: 'source/img/kapuchino.jpg',
+      kind: 'hot'
+    },
+    {
+      keyword: 'greentea',
+      name: 'Зеленый чай',
+      price: 100,
+      category: 'drink',
+      weight: '300 мл',
+      image: 'source/img/greentea.jpg',
+      kind: 'hot'
+    },
+    {
+      keyword: 'blacktea',
+      name: 'Черный чай',
+      price: 90,
+      category: 'drink',
+      weight: '300 мл',
+      image: 'source/img/blacktea.jpg',
+      kind: 'hot'
+    },
+    {
+      keyword: 'pahlava',
+      name: 'Пахлава',
+      price: 220,
+      category: 'dessert',
+      weight: '300 г',
+      image: 'source/img/pahlava.jpg',
+      kind: 'small'
+    },
+    {
+      keyword: 'cheesecake',
+      name: 'Чизкейк',
+      price: 240,
+      category: 'dessert',
+      weight: '125 г',
+      image: 'source/img/cheesecake.jpg',
+      kind: 'small'
+    },
+    {
+      keyword: 'chocolate_cheesecake',
+      name: 'Шоколадный чизкейк',
+      price: 260,
+      category: 'dessert',
+      weight: '125 г',
+      image: 'source/img/chocolate_cheesecake.jpg',
+      kind: 'medium'
+    },
+    {
+      keyword: 'chocolate_cake',
+      name: 'Шоколадный торт',
+      price: 270,
+      category: 'dessert',
+      weight: '140 г',
+      image: 'source/img/chocolate_cake.jpg',
+      kind: 'medium'
+    },
+    {
+      keyword: 'donuts3',
+      name: 'Пончики (3 штуки)',
+      price: 410,
+      category: 'dessert',
+      weight: '350 г',
+      image: 'source/img/donuts3.jpg',
+      kind: 'small'
+    },
+    {
+      keyword: 'donuts6',
+      name: 'Пончики (6 штук)',
+      price: 650,
+      category: 'dessert',
+      weight: '700 г',
+      image: 'source/img/donuts6.jpg',
+      kind: 'big'
+    },
+  ];
---
+++ source/script.js
@@ -0,0 +1,291 @@
+import { products } from './dishes.js'; 
+
+document.addEventListener('DOMContentLoaded', () => {
+    const sections = {
+        soup: document.querySelector('.soups .gridContainer'),
+        maindish: document.querySelector('.maindishes .gridContainer'),
+        drink: document.querySelector('.drinks .gridContainer'),
+        salad: document.querySelector('.salads .gridContainer'),
+        dessert: document.querySelector('.desserts .gridContainer'),
+    };
+
+    const orderInfo = {
+        selectedSoup: null,
+        selectedMainDish: null,
+        selectedSalad: null,
+        selectedDrink: null,
+        selectedDessert: null,
+    };
+
+    document.querySelector('.buttonForm1').addEventListener('click', () => {
+        orderInfo.selectedSoup = null;
+        orderInfo.selectedMainDish = null;
+        orderInfo.selectedSalad = null;
+        orderInfo.selectedDrink = null;
+        orderInfo.selectedDessert = null;
+    
+        updateOrderDisplay();
+
+        document.querySelector('.selectedSoup').textContent = 'Блюдо не выбрано';
+        document.querySelector('.selectedMaindish').textContent = 'Блюдо не выбрано';
+        document.querySelector('.selectedSalad').textContent = 'Блюдо не выбрано';
+        document.querySelector('.selectedDrink').textContent = 'Блюдо не выбрано';
+        document.querySelector('.selectedDessert').textContent = 'Блюдо не выбрано';
+    
+
+        document.querySelectorAll('.orderCategory').forEach(category => {
+            category.style.display = 'block';
+        });
+    
+        const totalPriceEl = document.querySelector('.totalPrice');
+        totalPriceEl.textContent = '';
+        totalPriceEl.style.display = 'none';
+    });
+    
+    function renderDishes() {
+        const soups = products.filter(product => product.category === 'soup');
+        const maindishes = products.filter(product => product.category === 'maindish');
+        const salads = products.filter(product => product.category === 'salad');
+        const drinks = products.filter(product => product.category === 'drink');
+        const desserts = products.filter(product => product.category === 'dessert');
+
+        displayProducts(soups, sections.soup);
+        displayProducts(maindishes, sections.maindish);
+        displayProducts(salads, sections.salad);
+        displayProducts(drinks, sections.drink);
+        displayProducts(desserts, sections.dessert);
+    }
+
+    function displayProducts(products, section) {
+        section.innerHTML = ''; 
+        products.forEach(product => {
+            const dishDiv = document.createElement('div');
+            dishDiv.classList.add('dish');
+            dishDiv.setAttribute('dataDish', product.keyword);
+            dishDiv.setAttribute('dataKind', product.kind);
+            dishDiv.innerHTML = `
+                <img src="${product.image}" alt="${product.name}"/>
+                <p class="price">${product.price} ₽</p>
+                <p class="name">${product.name}</p>
+                <p class="weight">${product.weight}</p>
+                <button class="addToOrder">Добавить</button>
+            `;
+            section.appendChild(dishDiv);
+        });
+        attachEventListeners();
+    }
+
+
+    document.querySelectorAll(".categoryButtons button").forEach(filterButton => {
+        filterButton.addEventListener("click", (event) => {
+            const section = event.target.closest("section");
+            const previouslyActive = section.querySelector(".categoryButtons button.active");
+            
+            if (previouslyActive === event.target) {
+                event.target.classList.remove("active");
+                filterDishes(section, "all");
+            } else {
+                section.querySelectorAll(".categoryButtons button").forEach(btn => btn.classList.remove("active"));
+                event.target.classList.add("active");
+                const kind = event.target.classList[0];
+                filterDishes(section, kind);
+            }
+        });
+    });
+
+    const filterDishes = (section, kind) => {
+        const sectionDishes = section.querySelectorAll(".dish");
+        sectionDishes.forEach(dishElement => {
+            const dishKind = dishElement.getAttribute("dataKind");
+            if (kind === "all" || dishKind === kind) {
+                dishElement.style.display = "block";
+            } else {
+                dishElement.style.display = "none";
+            }
+        });
+    };
+
+    function attachEventListeners() {
+        const dishButtons = document.querySelectorAll('.addToOrder');
+        dishButtons.forEach(button => {
+            button.addEventListener('click', () => {
+                const dishDiv = button.parentElement;
+                const name = dishDiv.querySelector('.name').textContent;
+                const price = parseFloat(dishDiv.querySelector('.price').textContent);
+                const category = getCategory(dishDiv);
+
+                updateOrderInfo(category, name, price);
+                updateOrderDisplay();
+            });
+        });
+    }
+
+    function getCategory(dishDiv) {
+        if (dishDiv.closest('.soups')) return 'soup';
+        if (dishDiv.closest('.maindishes')) return 'maindish';
+        if (dishDiv.closest('.salads')) return 'salad';
+        if (dishDiv.closest('.drinks')) return 'drink';
+        if (dishDiv.closest('.desserts')) return 'dessert';
+    }
+
+    function updateOrderInfo(category, name, price) {
+        switch (category) {
+            case 'soup':
+                orderInfo.selectedSoup = { name, price };
+                break;
+            case 'maindish':
+                orderInfo.selectedMainDish = { name, price };
+                break;
+            case 'salad':
+                orderInfo.selectedSalad = { name, price };
+                break;
+            case 'drink':
+                orderInfo.selectedDrink = { name, price };
+                break;
+            case 'dessert':
+                orderInfo.selectedDessert = { name, price };
+                break;
+        }
+    }
+
+    const checkLunchComposition = () => {
+        const selectedCategories = Object.keys(orderInfo).filter(
+            key => orderInfo[key] !== null // 
+        ).map(key => {
+            if (key === "selectedSoup") return "soup";
+            if (key === "selectedMainDish") return "maindish";
+            if (key === "selectedSalad") return "salad";
+            if (key === "selectedDrink") return "drink";
+            if (key === "selectedDessert") return "dessert";
+        });
+    
+        const validCombos = [
+            ["soup", "maindish", "salad", "drink"],
+            ["salad", "maindish", "drink"],
+            ["soup", "salad", "drink"],
+            ["maindish", "salad", "drink"],
+            ["maindish", "drink"],
+        ];
+    
+        const isValidCombo = validCombos.some(combo =>
+            combo.every(item => selectedCategories.includes(item))
+        );
+
+        if (isValidCombo) {
+            //showAlert("...")
+            return true; 
+        }
+        else if (selectedCategories.length === 0) {
+            showAlert("Ничего не выбрано. Выберите блюда для заказа");
+            return false;
+        } else if (selectedCategories.includes("dessert") && selectedCategories.length === 1) {
+            showAlert("Выберите главное блюдо");
+            return false;
+        } else if (
+            selectedCategories.includes("maindish") &&
+            selectedCategories.includes("salad") &&
+            !selectedCategories.includes("drink") &&
+            selectedCategories.includes("soup")
+        ) {
+            showAlert("Выберите напиток");
+            return false;
+        } else if (
+            selectedCategories.includes("soup") &&
+            !selectedCategories.includes("maindish") &&
+            !selectedCategories.includes("salad")
+        ) {
+            showAlert("Выберите главное блюдо, салат или стартер");
+            return false;
+        } else if (
+            selectedCategories.includes("salad") &&
+            !selectedCategories.includes("soup") &&
+            !selectedCategories.includes("maindish")
+        ) {
+            showAlert("Выберите суп или главное блюдо");
+            return false;
+        } else if (
+            selectedCategories.includes("drink") &&
+            !selectedCategories.includes("maindish") &&
+            !selectedCategories.includes("salad")
+        ) {
+            showAlert("Выберите главное блюдо");
+            return false;
+        } /*else if (!isValidCombo) {
+            showAlert("Выберите все необходимые блюда для заказа");
+            return false;
+        }*/
+    };
+
+    const showAlert = (message) => {
+        if (document.querySelector(".alert")) {
+            return; 
+        }
+
+        const alertBox = document.createElement("div");
+        alertBox.classList.add("alert"); 
+        alertBox.innerHTML = `
+            <div class="alertContent">
+                <p>${message}</p>
+                <button class="closeAlert">Окей 👌</button>
+            </div>
+        `;
+
+    
+        document.body.style.overflow = "hidden";
+
+        document.body.appendChild(alertBox);
+
+        const closeButton = alertBox.querySelector(".closeAlert");
+        closeButton.addEventListener("click", () => {
+            alertBox.remove(); 
+            document.body.style.overflow = ""; 
+        });
+    };
+
+    document.querySelector('.buttonForm2').addEventListener('click', (event) => {
+        event.preventDefault(); //
+    
+        if (checkLunchComposition()) {
+            console.log("Заказ отправлен!"); 
+        } else {
+            console.log("Не удалось отправить заказ. Проверьте состав.");
+        }
+    });
+
+    function updateOrderDisplay() {
+        const selectedSoupEl = document.querySelector('.selectedSoup');
+        const selectedMainDishEl = document.querySelector('.selectedMaindish');
+        const selectedSaladEl = document.querySelector('.selectedSalad');
+        const selectedDrinkEl = document.querySelector('.selectedDrink');
+        const selectedDessertEl = document.querySelector('.selectedDessert');
+        const totalPriceEl = document.querySelector('.totalPrice');
+    
+        const isAnythingSelected = orderInfo.selectedSoup || orderInfo.selectedMainDish || orderInfo.selectedSalad || orderInfo.selectedDrink || orderInfo.selectedDessert;
+    
+        document.querySelectorAll('.orderCategory').forEach(category => {
+            category.style.display = isAnythingSelected ? 'block' : 'none';
+        });
+    
+        selectedSoupEl.textContent = orderInfo.selectedSoup ? `${orderInfo.selectedSoup.name} ${orderInfo.selectedSoup.price}₽` : 'Блюдо не выбрано';
+        selectedMainDishEl.textContent = orderInfo.selectedMainDish ? `${orderInfo.selectedMainDish.name} ${orderInfo.selectedMainDish.price}₽` : 'Блюдо не выбрано';
+        selectedSaladEl.textContent = orderInfo.selectedSalad ? `${orderInfo.selectedSalad.name} ${orderInfo.selectedSalad.price}₽` : 'Блюдо не выбрано';
+        selectedDrinkEl.textContent = orderInfo.selectedDrink ? `${orderInfo.selectedDrink.name} ${orderInfo.selectedDrink.price}₽` : 'Блюдо не выбрано';
+        selectedDessertEl.textContent = orderInfo.selectedDessert ? `${orderInfo.selectedDessert.name} ${orderInfo.selectedDessert.price}₽` : 'Блюдо не выбрано';
+
+        const totalPrice = (orderInfo.selectedSoup?.price || 0) + (orderInfo.selectedMainDish?.price || 0) + (orderInfo.selectedSalad?.price || 0) + (orderInfo.selectedDrink?.price || 0) + (orderInfo.selectedDessert?.price || 0);
+        totalPriceEl.textContent = `Стоимость заказа: ${totalPrice} ₽`;
+    
+        if (!isAnythingSelected) {
+            selectedSoupEl.textContent = 'Блюдо не выбрано';
+            selectedMainDishEl.textContent = 'Блюдо не выбрано';
+            selectedSaladEl.textContent = 'Блюдо не выбрано';
+            selectedDrinkEl.textContent = 'Блюдо не выбрано';
+            selectedDessertEl.textContent = 'Блюдо не выбрано';
+            totalPriceEl.style.display = 'none';
+        } else {
+            totalPriceEl.style.display = 'block';
+        }
+    }
+    
+    renderDishes();
+});
